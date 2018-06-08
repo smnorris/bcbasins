57493d0f525c0ddf433f7e0595fb50ca32349d74
Simon Norris <snorris@hillcrestgeo.ca>
2018-06-08 10:53:43 -0700

    fix name of shapefiles

--- 05_refine_watersheds_arcgis.py
+++ 05_refine_watersheds_arcgis.py
@@ -1,8 +1,8 @@
 from fwakit import watersheds_arcgis
 
 watersheds_arcgis.wsdrefine_dem(
-    r'data\prelimwsd_hex.shp',
-    r'data\prelimwsd_streams.shp',
+    r'data\wsdrefine_hex.shp',
+    r'data\wsdrefine_streams.shp',
     'station',
     r'Q:\dsswhse\Data\Base\DEMs Hillshades\Base20\BC DEM.gdb\bc_dem',
     in_mem=True
